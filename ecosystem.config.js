2968cb5d2aa9ad6d3a2ccbe4f5ebd35c5912b753
josephvillalba96 <dev.josephvillalba@gmail.com>
2025-12-18 12:24:16 -0500

    cambios en el eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
         cwd: "/home/ubuntu/DflayerApi",
   
         script: ".venv/bin/uvicorn",
-        args: "app.main:app --host 127.0.0.1 --port 8000",
+        args: "app.main:app --host 127.0.0.1 --port 8000", //debe ser app.main:app
   
         interpreter: "none",
         exec_mode: "fork",
